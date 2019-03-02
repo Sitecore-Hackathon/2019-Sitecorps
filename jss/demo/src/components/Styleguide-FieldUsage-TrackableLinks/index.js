44d6aaf0541e31196acc9d5a832061addcb896e1
James Gregory <james.gregory@americaneagle.com>
2019-03-02 14:26:00 -0600

    Highlight JSS

--- jss/demo/src/components/Styleguide-FieldUsage-TrackableLinks/index.js
+++ jss/demo/src/components/Styleguide-FieldUsage-TrackableLinks/index.js
@@ -1,13 +1,17 @@
+/*
+// Example Usage
+*/
 import React from 'react';
 import { Link } from '@sitecore-jss/sitecore-jss-react';
-import {TrackableLink} from '../Sitecorps-react-trackable-link/TrackableLink'
 import StyleguideSpecimen from '../Styleguide-Specimen';
-
+// Import new module to use in place of Sitecore <Link />:
+import {TrackableLink} from '../Sitecorps-react-trackable-link/TrackableLink'
 
 const StyleguideFieldUsageTrackableLinks = (props) => (
   <StyleguideSpecimen {...props} e2eId="styleguide-fieldusage-trackablelinks">
     Select what best describe you!
 	<br />
+     
     <TrackableLink field={props.fields.LinkA} />
     <br />
     <TrackableLink field={props.fields.LinkB}>
